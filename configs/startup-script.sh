9509d255ac62714f3d326a56640fd4b8502b43d6
anisriva <animesh.srivastava@digicert.com>
2021-08-28 22:08:16 +0530

    breakthrough

--- configs/startup-script.sh
+++ configs/startup-script.sh
@@ -15,14 +15,14 @@ chmod +x $DEBEZIUM_HOME/config.properties
 chmod +x $DEBEZIUM_HOME/inventory.cql
 chmod +x $DEBEZIUM_HOME/log4j.properties
 chmod +x $DEBEZIUM_HOME/startup-script.sh
-chmod +x $CASSANDRA_YAML/cassandra.yaml
+# chmod +x $CASSANDRA_YAML/cassandra.yaml
 chmod +x $DEBEZIUM_HOME/debezium-connector-cassandra.jar
 
 chown -R dse:dse $DEBEZIUM_HOME/config.properties $DEBEZIUM_HOME
 chown -R dse:dse $DEBEZIUM_HOME/inventory.cql $DEBEZIUM_HOME
 chown -R dse:dse $DEBEZIUM_HOME/log4j.properties $DEBEZIUM_HOME
 chown -R dse:dse $DEBEZIUM_HOME/startup-script.sh $DEBEZIUM_HOME
-chown -R dse:dse $CASSANDRA_YAML/cassandra.yaml $DEBEZIUM_HOME
+# chown -R dse:dse $CASSANDRA_YAML/cassandra.yaml $DEBEZIUM_HOME
 chown -R dse:dse $DEBEZIUM_HOME/debezium-connector-cassandra.jar $DEBEZIUM_HOME
 echo "Files copied to $DEBEZIUM_HOME"
 cd $DEBEZIUM_HOME
@@ -33,10 +33,16 @@ do
   echo 'Waiting for DSE to start'
 done;
 
+for i in `cqlsh -u cassandra -p cassandra -e "desc full schema" | grep -i everywhere | awk '{print $3}'`;
+do
+  cqlsh -u cassandra -p cassandra -e  "alter KEYSPACE $i WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1};";
+  echo "Converted keyspace:$i to SimpleStrategy from EverywhereStratergy"
+done
+
 cqlsh -f $DEBEZIUM_HOME/inventory.cql
 echo 'Testdb Created now starting debezium producer'
 
-java -Dlog4j.debug \
+nohup java -Dlog4j.debug \
 -Dlog4j.configuration=file:$DEBEZIUM_HOME/log4j.properties \
 -jar $DEBEZIUM_HOME/debezium-connector-cassandra.jar \
-$DEBEZIUM_HOME/config.properties >  $DEBEZIUM_HOME/debezium.stdout.log 2> $DEBEZIUM_HOME/debezium.stderr.log
+$DEBEZIUM_HOME/config.properties >  $DEBEZIUM_HOME/debezium.stdout.log 2> $DEBEZIUM_HOME/debezium.stderr.log &
